932fce23c010e7c5c09885b4c0b512837077ce4e
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-10-24 17:55:20 -0700

    added im2col test

--- src/test.c
+++ src/test.c
@@ -8,6 +8,7 @@
 #include "image.h"
 #include "test.h"
 #include "args.h"
+#include "uwnet.h"
 
 double what_time_is_it_now()
 {
@@ -38,9 +39,28 @@ void test_matrix_speed()
     printf("Transpose elapsed %lf sec\n", what_time_is_it_now() - start);
 }
 
+void test_im2col() {
+    float input_data[] = {
+        1,2,3,
+        4,5,6
+    };
+
+    // matrix x = make_matrix(2,3);
+    image example = float_to_image(input_data, 3, 2, 1);
+    matrix x = im2col(example, 3, 2);
+    for (int i = 0; i < x.rows; i++) {
+        printf("");
+        for (int j =0; j < x.cols; j++) {
+            printf(", %f", x.data[i * x.cols + j]);
+        }
+        printf("\n");
+    }
+}
+
 void run_tests()
 {
     test_matrix_speed();
+    test_im2col();
     //printf("%d tests, %d passed, %d failed\n", tests_total, tests_total-tests_fail, tests_fail);
 }
 
